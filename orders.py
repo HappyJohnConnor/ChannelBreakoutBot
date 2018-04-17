d14b37f121e4b6fb390dbda85bb2d283c76f59cb
OmenByHp <tomoakitaro777@gmail.com>
2018-04-17 23:01:20 +0900

    second

---
+++ orders.py
@@ -0,0 +1,53 @@
+import json
+import traceback
+import inspect
+import time
+
+
+class Orders:
+    def __init__(self, market):
+        f = open('config.json', 'r', encoding="utf-8")
+        config = json.load(f)
+        self.market = market
+        self.product_code = config['productCode']
+
+    def limit(self, side, price, size):
+        return self.market.create_order(self.product_code, type='limit', side=side, price=price, amount=size)
+
+    def market(self, side, size):
+        return self.market.create_order(self.product_code,
+                                        type='market', side=side, amount=size)
+
+    def make_order(self, side, size):
+        closeid = ''
+        error_times = 0
+        last = self.market.fetch_ticker(self.product_code)['last']
+        while True:
+            try:
+                if closeid == '':
+                    order = self.limit(side, last, size)
+                    closeid = order['id']
+                else:
+                    # 15秒で確定できなかったら、成行
+                    positions = self.market.private_get_position()[1]
+                    if positions['openOrderSellQty'] != 0 or positions['openOrderBuyQty'] != 0:
+                        self.market.cancel_order(closeid)
+                        self.market(side, size)
+                        print(side + ' by market')
+                        break
+                    else:
+                        print(side+' by limit')
+                        break
+
+                error_times = 0
+                time.sleep(10)
+            except:
+                print('エラーが発生しました。')
+                print("Place is " + inspect.currentframe().f_code.co_name)
+                traceback.print_exc()
+                error_times += 1
+                if error_times <= 10:
+                    continue
+                else:
+                    time.sleep(10)
+                    continue
---
+++ trade.py
@@ -0,0 +1,148 @@
+import time
+import ccxt
+import pytz
+import inspect
+import orders
+import json
+from datetime import datetime
+
+candle_duct = {'1m': 1, '5m': 5, '1h': 60}
+
+bitmex = ccxt.bitmex({
+    'apiKey': 'your key',
+    'secret': 'secret',
+})
+
+f = open('config.json', 'r', encoding="utf-8")
+config = json.load(f)
+product_code = config['productCode']
+candle_term = config['candleTerm']
+period = config['period']
+if config['isTest']:
+    apiKey = config['test']['key']
+    secret = config['test']['secret']
+    bitmex = ccxt.bitmex({
+        'apiKey': apiKey,
+        'secret': secret,
+    })
+    bitmex.urls['api'] = bitmex.urls['test']
+else:
+    apiKey = config['real']['key']
+    secret = config['real']['secret']
+    bitmex = ccxt.bitmex({
+        'apiKey': apiKey,
+        'secret': secret,
+    })
+lot = config['lot']
+orders = orders.Orders(bitmex)
+
+
+def main():
+
+    balance = get_balance()
+    print('Balance:' + str(balance))
+
+    pos = frist_order(period, candle_term)
+    print('came back to main\n')
+    while True:
+        judge = channel_break(period, candle_term)
+
+        if judge == 1 and pos == -1:
+            print("Buy!!")
+            orders.make_order('buy', lot)
+
+        elif judge == 0 and pos == 1:
+            print("Sell!!")
+            orders.make_order('sell', lot)
+        print('\n')
+        time.sleep(30)  # 一定時間(秒)待つ
+
+
+def frist_order(period, candle_term):
+    pos = 1
+    while True:
+        judge = channel_break(period, candle_term)
+        if judge == 1:
+            print("Buy!!")
+            orders.make_order('buy', lot / 2)
+            pos = 1
+            break
+        elif judge == 0:
+            print("Sell!!")
+            orders.make_order('sell', lot / 2)
+            pos = -1
+            break
+        print('\n')
+        time.sleep(30)
+    return pos
+
+
+def get_balance():
+    try:
+        balance = bitmex.fetch_balance(
+        )['BTC']['free'] * bitmex.fetch_ticker('BTC/USD')['close']
+    except IndexError as error:
+        print('エラーが発生しました。')
+        print("Place is " + inspect.currentframe().f_code.co_name)
+        balance = 0
+    return balance
+
+
+def channel_break(period, candle_term):
+    candle_num = candle_duct[candle_term]
+    judge = 0
+    error_times = 0
+    while True:
+        try:
+            print(time_is())
+            timestamp = round(datetime.now().timestamp()*1000)
+            past_time = timestamp - (period+1) * candle_num * 60000  # 1分は60000
+            multi_candles = bitmex.fetch_ohlcv(
+                product_code, timeframe=candle_term, since=past_time)
+
+            highest = max([multi_candles[i][2] for i in range(period - 1)])
+            lowest = max([multi_candles[i][3] for i in range(period - 1)])
+            print('highest:' + str(highest))
+            print('lowest:' + str(lowest))
+
+            before_1min = timestamp - candle_num * 60000  # 1分は60000
+            candle = bitmex.fetch_ohlcv(
+                product_code, timeframe=candle_term, since=before_1min)
+            high = candle[0][2]
+            low = candle[0][3]
+
+            print('high:' + str(high))
+            print('low:'+str(low))
+
+            if highest < high:
+                judge = 1
+                break
+            if lowest > low:
+                judge = 0
+                break
+            time.sleep(30)
+
+        except:
+            print('エラーが発生しました。')
+            print("Place is " + inspect.currentframe().f_code.co_name)
+            traceback.print_exc()
+            error_times += 1
+            if error_times <= 10:
+                continue
+            else:
+                time.sleep(10)
+                continue
+    return judge
+
+
+def time_is():
+    now = datetime.now()
+    tokyo_timezone = pytz.timezone('Asia/Tokyo')
+    tokyo_datetime = tokyo_timezone.localize(now)
+
+    return tokyo_datetime.strftime('%Y/%m/%d %H:%M:%S')
+
+
+if __name__ == '__main__':
+    main()
+    # candle_term:1h, 1m,5m,
